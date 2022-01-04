527766b3b317ea8b0e7646c75437e6789bd6d3a9
Noah Bissell <s015470@students.lmsd.org>
2022-01-04 12:10:58 -0500

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Noah Bissell (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
