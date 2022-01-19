2e28b254a3a0478b9f88d2d8b721e84f4dda5da7
George Johann Luigi Lauri <s015915@students.lmsd.org>
2022-01-19 14:35:04 -0500

    KFI Image

--- Recipes/IndividualRecipeView.swift
+++ Recipes/IndividualRecipeView.swift
@@ -6,13 +6,14 @@
 //
 
 import SwiftUI
+import struct Kingfisher.KFImage
 
 struct IndividualRecipeView: View {
     var recipe : FetchRecipe
     
     var body: some View {
         Text(recipe.recipe.title ?? "Error loading")
-        KFImage(recipeResult.recipe.image)
+        KFImage(recipe.recipe.image)
         Text(recipe.recipe.summary ?? "Error loading")
         if let mins = recipe.recipe.readyInMinutes{
             Text("Ready in \(mins) minutes.")
