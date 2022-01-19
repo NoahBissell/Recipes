7dc7ce1c1887d0b0b35f4f9d0454fff12ae4a589
Noah Bissell <s015470@students.lmsd.org>
2022-01-19 14:28:48 -0500

    nothing

--- Recipes/IndividualRecipeView.swift
+++ Recipes/IndividualRecipeView.swift
@@ -11,7 +11,12 @@ struct IndividualRecipeView: View {
     var recipe : FetchRecipe
     
     var body: some View {
-        Text("Placeholder")
+        Text(recipe.recipe.title ?? "Error loading")
+        KFImage(recipeResult.recipe.image)
+        Text(recipe.recipe.summary ?? "Error loading")
+        if let mins = recipe.recipe.readyInMinutes{
+            Text("Ready in \(mins) minutes.")
+        }
     }
 }
 
