bbb982988c488a64b2ac6163eb7341c7f5e1b7e1
George Johann Luigi Lauri <s015915@students.lmsd.org>
2022-01-21 12:00:39 -0500

    Changed content view

--- Recipes/ContentView.swift
+++ Recipes/ContentView.swift
@@ -1,53 +1,78 @@
-//
 //  ContentView.swift
 //  Recipes
-//
-//  Created by Noah Bissell (student LM) on 1/4/22.
-//
+//  Created by Liam Shack (student LM) on 1/4/22.
 
 import SwiftUI
+
+var vegan = FetchData(diet : "vegan")
+var vegetarian = FetchData(diet : "vegetarian")
+var gluten = FetchData(diet : "gluten")
+var paleo = FetchData(diet : "paleo")
+var pescetarian = FetchData(diet : "pescetarian")
+struct ImageOverlay: View{
+    var body: some View{
+        VStack{
+        ZStack{
+            NavigationView {
+                List{
+                    Spacer(minLength: 200)
+                    NavigationLink(
+                        destination: RecipeListView(fetchRecipes: vegan),
+                        label: {
+                            Text("Vegan")
+                        })
+                    NavigationLink(
+                        destination: RecipeListView(fetchRecipes: vegetarian),
+                        label: {
+                            Text("Vegetarian")
+                        })
+                    NavigationLink(
+                        destination: RecipeListView(fetchRecipes: gluten),
+                        label: {
+                            Text("Gluten Free")
+                        })
+                    NavigationLink(
+                        destination: RecipeListView(fetchRecipes: paleo),
+                        label: {
+                            Text("Paleo")
+                        })
+                    NavigationLink(
+                        destination: RecipeListView(fetchRecipes: pescetarian),
+                        label: {
+                            Text("Pescetarian")
+                        })
+                }
+            }//End of NavigationView
+        }.background(Color.black)
+        .opacity(0.5)
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         VStack{
-            Spacer(minLength: 75)
-            Text("Recipes")
-        NavigationView {
-            List{
-                NavigationLink(
-                    destination: RecipeListView(fetchRecipes: FetchData(diet : "vegan")),
-                    label: {
-                        Text("Vegan")
-                    })
-                NavigationLink(
-                    destination: RecipeListView(fetchRecipes: FetchData(diet : "vegetarian")),
-                    label: {
-                        Text("Vegetarian")
-                    })
-                NavigationLink(
-                    destination: RecipeListView(fetchRecipes: FetchData(diet : "gluten")),
-                    label: {
-                        Text("Gluten Free")
-                    })
-                NavigationLink(
-                    destination: RecipeListView(fetchRecipes: FetchData(diet : "paleo")),
-                    label: {
-                        Text("Paleo")
-                    })
-                NavigationLink(
-                    destination: RecipeListView(fetchRecipes: FetchData(diet : "pescetarian")),
-                    label: {
-                        Text("Pescetarian")
-                    })
+            Spacer(minLength: 80)
+            HStack(){
+                Spacer(minLength: 5)
+                Image("Homebackground")
+                    .resizable()
+                    .frame(width: 400, height: 950)
+                    .overlay(ImageOverlay(), alignment: .bottomTrailing)
+                Spacer()
+                Divider()
             }
         }
-        
-    }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+
