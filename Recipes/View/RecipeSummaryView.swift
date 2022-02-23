39e7aebc4b25aa4de19c695868f95bb944538805
eric's school computer <s017217@students.lmsd.org>
2022-02-23 13:21:50 -0500

    organizations

---
+++ Recipes/View/RecipeSummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeSummaryView.swift
+//  Recipes
+//
+//  Created by Eric Nie (student LM) on 2/23/22.
+//
+
+import SwiftUI
+
+struct RecipeSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeSummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeSummaryView()
+    }
+}
