9ff411d3bd4dd9a644ee8a1a23da39fd7bdd7901
Noah Bissell <s015470@students.lmsd.org>
2022-03-10 11:59:08 -0500

    Added a base for settings view from slowmograham, added color constants

---
+++ Recipes/Model/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SlowmoGraham
+//
+//  Created by Noah Bissell (student LM) on 2/2/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("backgroundColor")
+    static let buttonBackground = Color("buttonBackground")
+    static let buttonText = Color("buttonText")
+}
--- Recipes/OtherProjectViews/SettingsView.swift
+++ Recipes/OtherProjectViews/SettingsView.swift
@@ -8,13 +8,64 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @EnvironmentObject var userInfo : UserInfo
+    @State private var showSheet = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color.background)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image(uiImage: userInfo.image)
+                    .resizable()
+                    .frame(width:200, height: 200)
+                    .aspectRatio(contentMode: .fill)
+                    .clipShape(Circle())
+                HStack {
+                    Image(systemName: "person")
+                    TextField("name", text: $userInfo.name)
+                        .autocapitalization(.none)
+                        .keyboardType(.emailAddress)
+                        .padding()
+                        .disableAutocorrection(true)
+                        .onChange(of: userInfo.name, perform: { _ in
+                            FirebaseFunctions.addUsername(username: userInfo.name) { _ in}
+                        })
+                }
+                
+                Button("change image") {
+                    showSheet = true
+                }
+                .frame(width: UIScreen.main.bounds.width - 100)
+                .padding()
+                .background(Color.buttonBackground)
+                .foregroundColor(Color.buttonText)
+                .cornerRadius(30)
+                Button("sign out") {
+                    FirebaseFunctions.signOut(userInfo)
+                }
+                .frame(width: UIScreen.main.bounds.width - 100)
+                .padding()
+                .background(Color.buttonBackground)
+                .foregroundColor(Color.buttonText)
+                .cornerRadius(30)
+                
+                .padding()
+                }
+        }
+        .sheet(isPresented: $showSheet, onDismiss: {
+            FirebaseFunctions.uploadPicture(image: userInfo.image) { result in
+                //
+            }
+        }, content: {
+//            ImagePicker(selectedImage: $userInfo.image)
+        })
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        SettingsView().environmentObject(UserInfo())
     }
 }
